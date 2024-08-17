f0d5a29ff2b8499832365364bd9d336ed5f899f6
sofia-lpz <a01028251@tec.mx>
2024-08-16 20:25:02 -0600

    added photos final

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -11,18 +11,21 @@ import { dataProvider } from "./dataProvider";
 
 import { UserList } from "./users";
 import { PostEdit, PostList, PostCreate } from "./posts";
-import { TodoList, TodoEdit, TodoShow } from "./todos";
+import { TodoList, TodoEdit, TodoShow, TodoCreate } from "./todos";
 import { CommentList, CommentEdit, CommentShow } from "./comments";
 import { AlbumList, AlbumEdit, AlbumShow } from "./albums";
+import { PhotoList, PhotoEdit, PhotoShow, PhotoCreate} from "./photos";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
     <Resource name="users" list={UserList} edit={EditGuesser} show={ShowGuesser}/>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
 
+    <Resource name="todos" list={TodoList} edit={TodoEdit} show={TodoShow} create={TodoCreate}/>
 
-    <Resource name="todos" list={TodoList} edit={TodoEdit} show={TodoShow}/>
     <Resource name="comments" list={CommentList} edit={CommentEdit} show={CommentShow}/>
     <Resource name="albums" list={AlbumList} edit={AlbumEdit} show={AlbumShow}/>
+
+    <Resource name="photos" list={PhotoList} edit={PhotoEdit} show={PhotoShow} create={PhotoCreate}/>
   </Admin>
 );
